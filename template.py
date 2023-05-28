ee22bf7686b56945dea71f1786b89ba018d4a2f4
omshivasai <shivasaigurrapu@gmail.com>
2023-05-28 16:38:42 +0530

    added requirements file and updated with setup.py file also for Pypi

--- template.py
+++ template.py
@@ -30,6 +30,14 @@ list_of_files = [
 
 ]
 
+'''In simple terms, the below  Python code is trying to create a predefined list of files and directories for a project.
+
+For each file path in the list:
+
+It checks if the file's directory exists. If not, it creates the directory.
+It checks if the file exists or if it is empty. If either condition is true, it creates an empty file.
+If the file already exists and is not empty, it logs a message indicating that the file already exists.'''
+
 
 for filepath in list_of_files:
     filepath = Path(filepath)
