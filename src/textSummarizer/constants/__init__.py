d23ee8261e6bfce0c2b6db13211ec67c4323a673
omshivasai <shivasaigurrapu@gmail.com>
2023-06-06 10:00:38 +0530

    completed data ingestion part

---
+++ src/textSummarizer/constants/__init__.py
@@ -0,0 +1,4 @@
+from pathlib import Path
+
+CONFIG_FILE_PATH = Path("/home/T7730OG/Documents/Text_Outlier_Project/config/config.yaml")
+PARAMS_FILE_PATH = Path("/home/T7730OG/Documents/Text_Outlier_Project/params.yaml")
