b83ed3a9e75127a4edb9db9b4d3677ecf2721311
omshivasai <shivasaigurrapu@gmail.com>
2023-06-07 16:32:26 +0530

    data_transformation

--- src/textSummarizer/entity/__init__.py
+++ src/textSummarizer/entity/__init__.py
@@ -16,4 +16,12 @@ class DataValidationConfig:
     STATUS_FILE: str
     ALL_REQUIRED_FILES: list
 
+
+
+@dataclass(frozen=True)
+class DataTransformationConfig:
+    root_dir: Path
+    data_path: Path
+    tokenizer_name: Path
+
     
