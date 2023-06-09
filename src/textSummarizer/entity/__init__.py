34cff9aa3ba12d2ae725d90cc89db899eceaa241
omshivasai <shivasaigurrapu@gmail.com>
2023-06-09 17:52:43 +0530

    modeltrainer and modelevaluation

--- src/textSummarizer/entity/__init__.py
+++ src/textSummarizer/entity/__init__.py
@@ -1,4 +1,3 @@
-# this is an entity func for data ingestion config class
 from dataclasses import dataclass
 from pathlib import Path
 
@@ -23,5 +22,31 @@ class DataTransformationConfig:
     root_dir: Path
     data_path: Path
     tokenizer_name: Path
+
+
+@dataclass(frozen=True)
+class ModelTrainerConfig:
+    root_dir: Path
+    data_path: Path
+    model_ckpt: Path
+    num_train_epochs: int
+    warmup_steps: int
+    per_device_train_batch_size: int
+    weight_decay: float
+    logging_steps: int
+    evaluation_strategy: str
+    eval_steps: int
+    save_steps: float
+    gradient_accumulation_steps: int
+
+
+
+@dataclass(frozen=True)
+class ModelEvaluationConfig:
+    root_dir: Path
+    data_path: Path
+    model_path: Path
+    tokenizer_path: Path
+    metric_file_name: Path
 
     
