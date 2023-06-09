eed0f67668434f88cb0fb52487c04148161c88ac
omshivasai <shivasaigurrapu@gmail.com>
2023-06-09 18:28:29 +0530

    prediction file and ui file

---
+++ src/textSummarizer/pipeline/prediction.py
@@ -0,0 +1,27 @@
+# the model trainer and model evaluator files are for checking the model performance on our prepared data but this file is for checking on new data
+
+from textSummarizer.config.configuration import ConfigurationManager
+from transformers import AutoTokenizer
+from transformers import pipeline
+
+
+class PredictionPipeline:
+    def __init__(self):
+        self.config = ConfigurationManager().get_model_evaluation_config()
+
+
+    
+    def predict(self,text):
+        tokenizer = AutoTokenizer.from_pretrained(self.config.tokenizer_path)
+        gen_kwargs = {"length_penalty": 0.8, "num_beams":8, "max_length": 128}
+
+        pipe = pipeline("summarization", model=self.config.model_path,tokenizer=tokenizer)
+
+        print("Dialogue:")
+        print(text)
+
+        output = pipe(text, **gen_kwargs)[0]["summary_text"]
+        print("\nModel Summary:")
+        print(output)
+
+        return output
